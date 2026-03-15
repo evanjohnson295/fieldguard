e3bf8f9210824040b9f9deb00cefb2a016c07662
evanjohnson295 <evanjohnson295@gmail.com>
2026-03-14 22:40:51 -0600

    Initial build

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import Hero from './components/Hero'
-import Problem from './components/Problem'
-import HowItWorks from './components/HowItWorks'
-import TechSpecs from './components/TechSpecs'
-import Footer from './components/Footer'
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Problem />
-      <HowItWorks />
-      <TechSpecs />
-      <Footer />
     </main>
   )
 }
